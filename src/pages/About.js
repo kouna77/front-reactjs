2ed5b1d2c35c6902d131b125a6c4795e4d8308fc
KOUNA FALL [SNT DSI/DAC/DIF/RAD] <TMP_FALL53278@orange-sonatel.com>
2021-12-28 14:53:27 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../Components/Navigation';
+
+const About = () => {
+    return (
+        <div className="about">
+            <Navigation/>
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import Navigation from "../Components/Navigation";
+
+const Home = () => {
+    return ( 
+        <div className="home">
+            <Navigation/>
+            <h1>Acceuil</h1>
+        </div>
+    );
+}
+export default Home;
